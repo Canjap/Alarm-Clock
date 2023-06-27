ea9ee0f6883d3ba3a292e1e1e7a3b90b940bedae
Sanjay <sanjaychemban@gmail.com>
2023-06-27 17:39:54 -0400

    added another page: AlarmsPage; use IconButton widget to add alarms?

--- alarm_clock_app/lib/main.dart
+++ alarm_clock_app/lib/main.dart
@@ -28,14 +28,73 @@ class MyAppState extends ChangeNotifier {
   var currentDateTime = DateTime.now();
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch(selectedIndex){
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = AlarmsPage();
+        break;
+      default:
+      throw UnimplementedError("no widget for $selectedIndex");
+    }
+
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (value) {
+                setState(() {
+                  selectedIndex = value;
+                });;
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: page,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var dateTimeNow = appState.currentDateTime;
     var dayNow = DateFormat('EEEE, d MMM, yyyy').format(dateTimeNow);
     var timeNow = DateFormat("jm").format(dateTimeNow);
-    
+
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -97,6 +156,24 @@ class displayTime extends StatelessWidget {
           padding: const EdgeInsets.all(20.0),
           child: Text("$timeNow", style: style,),
         ),
+      ),
+    );
+  }
+}
+
+class AlarmsPage extends StatelessWidget {
+  const AlarmsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            
+          ),
+        Text("data"),
+      ]
       ),
     );
   }
